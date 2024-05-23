b8edc84da1bca20b3da9c9c278352af133e38690
Nicharee Jewbuddhidham <nicharee@wellios.co.th>
2024-05-23 14:18:48 +0700

    method

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+type movie struct {
+	title       string
+	year        int
+	rating      float32
+	genres      []string
+	isSuperHero bool
+}
+
+func (m movie) info() {
+	fmt.Printf("%s (%d) - %.2f\n", m.title, m.year, m.rating)
+	fmt.Println("Genres:")
+	for _,g := range m.genres{
+		fmt.Printf("\t%s\n", g)
+	}
+}
+
 func main() {
-	fmt.Println("Hello Gopher")
+	ae := movie{
+		title:       "Avengers: Endgame",
+		year:        2019,
+		rating:      8.4,
+		genres:      []string{"Action", "Drama"},
+		isSuperHero: true,
+	}
+	ae.info()
 }
